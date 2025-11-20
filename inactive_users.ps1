d3544941fde0bec73bc8a1e06e04efdf1133f7fb
Janusz Kornath <190484075+JanuszKornath@users.noreply.github.com>
2025-11-20 12:19:54 +0100

    Add admin check to inactive_users.ps1

--- inactive_users.ps1
+++ inactive_users.ps1
@@ -1,3 +1,5 @@
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
+
 Import-Module ActiveDirectory
 
 # Anzahl der Tage ohne Anmeldung
